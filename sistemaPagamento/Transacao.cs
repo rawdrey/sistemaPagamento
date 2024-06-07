7673094b0fd038ac87cdc2b91b68da2b0332021f
Rawdrey <rawdrey@gmail.com>
2024-06-07 20:41:14 -0300

    implementado class transação, cartão de crédito, boleto, transferencia bancária

--- sistemaPagamento/Transacao.cs
+++ sistemaPagamento/Transacao.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace sistemaPagamento
 {
-    class Transacao
+    abstract class Transacao
     {
-    }
+        public string idTransacao { get; }
+        public decimal Valor { get; set; }
+        public DateTime DataTransacao { get; }
+        public Transacao(string idTransacao, decimal valor, DateTime dataTransacao)
+        {
+            IdTransacao = idTransacao;
+            Valor = valor;
+            DataTransacao = dataTransacao;
+        }
+        public virtual decimal CalcularTaxa()
+        {
+            return 0;
+        }
 }
--- sistemaPagamento/Transferência.cs
+++ sistemaPagamento/Transferência.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace sistemaPagamento
 {
     class Transferência
+: Transacao
     {
-    }
+        public string NumeroContaOrigem { get; set; }
+        public string NumeroContaDestino { get; set; }
+        public TransferenciaBancaria(string idTransacao, decimal valor, DateTime dataTransacao, string numeroContaOrigem, string numeroContaDestino)
+    : base(idTransacao, valor, dataTransacao)
+        {
+            NumeroContaOrigem = numeroContaOrigem;
+            NumeroContaDestino = numeroContaDestino;
+        }
+
+        public override decimal CalcularTaxa()
+        {
+            return 10.00m;
+        }
 }
